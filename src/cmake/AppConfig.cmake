b2922c068c16cfdd36bb8a8f89254ff8144c8e14
lineralpha <5386041+lineralpha@users.noreply.github.com>
2025-04-18 00:04:43 -0700

    move CMake scripts to dedicated folders

--- src/cmake/AppConfig.cmake
+++ src/cmake/AppConfig.cmake
@@ -1,3 +1,3 @@
 # configure a header file to pass CMake settings/options
 # the output file AppConfig.h will be written into build tree directory
-configure_file(AppConfig.h.in AppConfig.h)
+configure_file(cmake/AppConfig.h.in AppConfig.h)
